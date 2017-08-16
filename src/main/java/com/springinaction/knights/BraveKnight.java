e93a71c3a85b509fc38611752fa7f269a127ba41
liangsay <sonywuyang@163.com>
2017-08-16 23:56:00 +0800

    spring in action page15
    第15页

--- src/main/java/com/springinaction/knights/BraveKnight.java
+++ src/main/java/com/springinaction/knights/BraveKnight.java
@@ -5,12 +5,15 @@ package com.springinaction.knights;
  */
 public class BraveKnight implements Knight {
     private Quest quest;
+    private Minstrel minstrel;
 
-    public BraveKnight(Quest quest) {
+    public BraveKnight(Quest quest, Minstrel minstrel) {
         this.quest = quest;
+        this.minstrel = minstrel;
     }
 
-    public void embarkOnQuest() {
+
+    public void embarkOnQuest(){
         quest.embark();
     }
 }
